a8c033310f1b3f01561bf10e481a4ec52a5c25a7
YauheniPo <ip.popovich@mail.ru>
2019-02-24 20:06:50 +0300

    Updated pom

--- src/main/java/popo/flow/framework/config/TestFrameworkConfig.java
+++ src/main/java/popo/flow/framework/config/TestFrameworkConfig.java
@@ -1,21 +1,15 @@
 package popo.flow.framework.config;
 
 import lombok.extern.log4j.Log4j2;
-import org.openqa.selenium.remote.RemoteWebDriver;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.ImportResource;
-import popo.flow.framework.driver.Browser;
 
 @Log4j2
-@Configuration
-@ComponentScan("popo.flow.framework")
-@ImportResource("classpath:spring.xml")
+//@Configuration
+//@ComponentScan("popo.flow.framework")
+//@ImportResource("classpath:spring.xml")
 public class TestFrameworkConfig {
 
-    @Bean
-    public RemoteWebDriver webDriver() {
-        return Browser.getDriver();
-    }
+//    @Bean
+//    public RemoteWebDriver webDriver() {
+//        return Browser.getDriver();
+//    }
 }
