4550997e380a4661de2d3c6c4be9b06912d8e849
YauheniPo <ip.popovich@mail.ru>
2019-03-10 15:57:04 +0300

    Updated pom.xml for testing

--- src/main/java/popo/flow/Runner.java
+++ src/main/java/popo/flow/Runner.java
@@ -1,5 +1,6 @@
 package popo.flow;
 
+import epam.popovich.annotation.time.TrackTime;
 import lombok.extern.log4j.Log4j2;
 import org.apache.logging.log4j.ThreadContext;
 import org.testng.ITestNGListener;
@@ -20,6 +21,7 @@ import static popo.flow.framework.Contants.TESTS_SOURCE;
 @Log4j2
 public class Runner {
 
+    @TrackTime
     public static void main(String[] args) {
         ThreadContext.put(LOGGER_THREAD_CONTEXT, "");
 
